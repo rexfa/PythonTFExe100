089f3210bf5c12bb786c85c3117b7c7322b06c61
rexfa <rex_fa@163.com>
2019-03-25 18:10:56 +0800

    搭建图纸

--- exe00/README.md
+++ exe00/README.md
@@ -1,2 +1,3 @@
 1 Tensonflow Helloworld 
 2 Tensonflow 添加层
+3 Tensonflow log 添加搭建图纸
--- exe00/exeTF003.py
+++ exe00/exeTF003.py
@@ -19,3 +19,55 @@ def add_layer(inputs,in_size,out_size,activation_function=None):
         else:
             outputs = activation_function(Wx_plus_b)
         return outputs
+
+
+#make target data with some noise
+x_data = np.linspace(-1,1,300)[:,np.newaxis]
+noise = np.random.normal(0,0.05,x_data.shape)
+y_data = np.square(x_data)+0.5+noise
+
+# Define placeholder for inputs to the network
+with tf.name_scope('inputs'):
+    xs = tf.placeholder(tf.float32,[None,1],name='x_in')
+    ys = tf.placeholder(tf.float32,[None,1],name='y_in')
+
+# Add a hide layer
+l1 = add_layer(xs,1,10,tf.nn.relu)
+# Add an output layer
+prediction = add_layer(l1,10,1,None)
+
+# The lose between prediction and real data
+with tf.name_scope('loss'):
+    loss = tf.reduce_mean(tf.reduce_sum(tf.square(ys-prediction),reduction_indices=[1]))
+with tf.name_scope('train'):
+    train_step = tf.train.GradientDescentOptimizer(0.1).minimize(loss)
+
+# Session
+session = tf.Session()
+
+# writer logs
+writer = tf.summary.FileWriter("logs/",session.graph)
+
+inti = tf.initialize_all_variables()
+session.run(inti)
+
+# draw the real data              
+fig1 = plt.figure()
+ax =fig1.add_subplot(1,1,1)
+ax.scatter(x_data,y_data)
+plt.ion()
+plt.show()
+
+# train 2000 times
+for i in range(2000):
+    session.run(train_step,feed_dict={xs:x_data,ys:y_data})
+    if i%50==0:
+        try:
+            ax.lines.remove(lines[0])
+        except Exception:
+            pass
+        prediction_value = session.run(prediction,feed_dict={xs:x_data})
+
+        # show the prediction
+        lines= ax.plot(x_data,prediction_value,'r-',lw=5)
+        plt.pause(1)
